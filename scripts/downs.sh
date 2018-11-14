c6b6b4353ad5df0260d2ac910fedacf119acc872
anton <antonbilbaeno@gmail.com>
2018-11-14 14:13:52 -0800

    fixes to parameter checking

--- scripts/downs.sh
+++ scripts/downs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
+if ! [ $# -eq 1 ]; then
   printf "\nEnter name of sch branch you want to checkout.\n"
   echo "(ex)   'downs roam'   |   'downs matter'"
-    exit
+  exit 1
+else
+  echo "Branch Name: $1"
+  echo ""
 fi
 
 printf "\n---->\nDo you want to pull all submodules?\n"
@@ -45,4 +48,4 @@ cd ..
 printf "\nBACK TO MAIN:\n"
 printf "If there are changes below...\nThe latest for each submodule was pulled, but main repo was tracking the submodule at an earlier commit. In other words, now you probably have a new version of fp or sym. In the worst case, kicad-libraries were updated and you may have to re-associate FPs.\n\n"
 git status
-printf "\n\nYou can commit these changes now or later.\n"
+printf "\n\nIf there are changes... you can commit now or later.\n"
--- scripts/ups.sh
+++ scripts/ups.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
+if ! [ $# -eq 1 ]; then
   printf "\nNeed to write commit message to push to ALL submodules.\n"
   printf "(ex) ups 'stuff'\n\n"
-    exit
+  exit 1
 fi
 
 echo "Are you sure you want to push to all submodules?"
+echo "with commit message: $1"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) echo "Pushing..."; break;;
