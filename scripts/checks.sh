039bbfb35bcf37c47634418d2576192d69e43aa6
AB <4b11b4@gmail.com>
2019-01-27 15:00:21 -0800

    checks: better statuses

--- scripts/checks.sh
+++ scripts/checks.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 printf "\nMAIN:\n" 
-git status 
+git status -s
 
 printf "\nSCH:\n" 
 cd "sch"
-git status
+git status -s
 
 printf "\nSYM:\n" 
 cd "../sym" 
-git status
+git status -s
 
 printf "\nFP:\n" 
 cd "../fp" 
-git status
+git status -s
 
 cd ..
