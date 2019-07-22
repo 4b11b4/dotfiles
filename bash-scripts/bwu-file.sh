ff921c4ae28139944f3106c0598be07c16a58d6a
AB <4b11b4@gmail.com>
2019-07-21 22:02:12 -0700

    Finally get the bash script to work. The problem was that the export variable did not persist outside of the script. In order for it to persist, run the scrip with "source" instead of "sh" (eg "source file.sh" instead of "./file.sh")

--- bash-scripts/bwu-file.sh
+++ bash-scripts/bwu-file.sh
@@ -2,13 +2,9 @@
 
 # Helper for dealing with registering session key export variable
 
-#STDOUT="$(bw unlock)"
+#STDOUT="$("bw unlock")"
 eval "exec > >(tee temp.txt)"
-sleep 2
-eval "bw unlock $1"
-eval ""
-
-sleep 2
+eval "bw unlock"
 STDOUT="$(cat ~/pub/dotfiles/temp.txt)"
 
 printf "\n0. Bitwarden output:\n"
@@ -24,7 +20,7 @@ printf "\n2. Export session key.\n"
 eval "$EXPORT"
 
 printf "\n3. Check current BW_SESSION variable:\n"
-eval "export -p | grep BW"
+eval "export -p"
 
 #printf "\n4. STDOUT again\n"
 #STDOUT=`cat ~/pub/dotfiles/temp.txt`
