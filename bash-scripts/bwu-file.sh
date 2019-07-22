fffe6ce2f9eb802be61f7a602d4942deca43a072
AB <4b11b4@gmail.com>
2019-07-21 21:40:36 -0700

    Continue to attempt to make a helper for the Bitwarden CLI in exporting the session key variable. It appears to be failing. The vault does not get unlocked, even though the "export -p" seems to show the proper session key being registered. I think it has something to do with the way bash is piping the output. When running without the script, the bw-cli outputs color coded text. When being ran through the script, it does not have these colors. Something with piping the text is somehow impacting the functionality of "bw unlock" even though the variable is properly exported

--- bash-scripts/bwu-file.sh
+++ bash-scripts/bwu-file.sh
@@ -2,8 +2,17 @@
 
 # Helper for dealing with registering session key export variable
 
-STDOUT="$(bw unlock)"
-echo ""
+#STDOUT="$(bw unlock)"
+eval "exec > >(tee temp.txt)"
+sleep 2
+eval "bw unlock $1"
+eval ""
+
+sleep 2
+STDOUT="$(cat ~/pub/dotfiles/temp.txt)"
+
+printf "\n0. Bitwarden output:\n"
+echo "$STDOUT"
 
 printf "\n1. Retrive export command:\n"
 # Basic bash string manipulation.
@@ -16,3 +25,6 @@ eval "$EXPORT"
 
 printf "\n3. Check current BW_SESSION variable:\n"
 eval "export -p | grep BW"
+
+#printf "\n4. STDOUT again\n"
+#STDOUT=`cat ~/pub/dotfiles/temp.txt`
