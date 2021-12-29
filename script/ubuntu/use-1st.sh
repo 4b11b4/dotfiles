6a89145a7480bd37b0285dede8965d3ac36aada8
AB <4b11b4@gmail.com>
2021-12-29 14:56:24 -0800

    about curling

--- script/ubuntu/use-1st.sh
+++ script/ubuntu/use-1st.sh
@@ -1,2 +1,5 @@
 curl https://raw.githubusercontent.com/4b11b4/dotfiles/master/script/ubuntu/1st.sh | bash
+
+#curl script w args
 #curl https://github.com/4b11b4/dotfiles/blob/master/script/ubuntu/1st.sh | bash -s -- arg1 arg2
+#curl https://raw.githubusercontent.com/4b11b4/dotfiles/master/script/ubuntu/1st.sh | bash -s -- -M -N stable
