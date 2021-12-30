e4a1f4869f4fb1de1ca64c5f0c66d169ed71bfcb
AB <4b11b4@gmail.com>
2021-12-30 15:06:03 -0800

    rm dup

--- script/ubuntu/first.sh
+++
@@ -1,5 +0,0 @@
-git config --global user.email "4b11b4@gmail.com"
-git config --global user.name "ab"
-
-sudo apt install vim
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
