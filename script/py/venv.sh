2ce5e7fcd655f9b744e368fb5dabe76276e7ffef
AB <4b11b4@gmail.com>
2021-12-31 14:58:33 -0800

    more reqs for venv

--- script/py/venv.sh
+++ script/py/venv.sh
@@ -1,3 +1,7 @@
+# assumes py3.6+
+
+sudo apt install python3-pip 
+sudo apt install python3-venv
 # recommend use system pip for user pip install
 python3 -m pip install --user --upgrade pip
 python3 -m pip --version
@@ -5,9 +9,15 @@ python3 -m pip --version
 read -p "ctrl+c | press key to create venv named 'env' in current dir" varName
 
 python3 -m venv env
+source env/bin/activate # no work from script? at least, no (env) prefix @ CLI
+which python
 
 read -p "ctrl+c | press key to install requirements.txt" varName
 
 python3 -m pip install -r requirements.txt
 
 python3 -m pip freeze
+
+read -p "ctrl+c | press key to leave venv" varName
+
+deactivate
