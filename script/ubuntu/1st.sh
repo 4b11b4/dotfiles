9e3b5f18ac6157529078d08e79f301b2f2485ef2
AB <4b11b4@gmail.com>
2021-12-31 12:45:13 -0800

    vim-gtk for system clipboards

--- script/ubuntu/1st.sh
+++ script/ubuntu/1st.sh
@@ -1,13 +1,21 @@
-cd ~ #assume run from home
+cd ~ # assume run from home
+
+# basics
 sudo apt install git
 
+# vim
+sudo apt install vim
+sudo apt install vim-gtk # +clipboard
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# puter lang
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
+
+# .files
 mkdir pub
 git clone https://github.com/4b11b4/dotfiles.git pub/dotfiles
 ./pub/dotfiles/install
 
+# ssh > pw
 #ssh-keygen
 #gedit .ssh/id_rsa.pub
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
