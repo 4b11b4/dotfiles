7b90343ee33d44dae3b63f59b45b76ff8c51e6da
AB <4b11b4@gmail.com>
2022-01-04 15:46:10 -0800

    dep src

--- script/py/install-python-3.7.12-asdf.sh
+++ script/py/install-python-3.7.12-asdf.sh
@@ -1,3 +1,7 @@
+# deps from https://bugs.python.org/issue31652#msg321260
+sudo apt-get install -y build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev zlib1g-dev libssl-dev openssl libgdbm-dev libgdbm-compat-dev liblzma-dev libreadline-dev libncursesw5-dev libffi-dev uuid-dev
+
+# asdf is the way
 asdf plugin add python
 asdf install python 3.7.12
-asfd global python 3.7.12
+asdf global python 3.7.12
