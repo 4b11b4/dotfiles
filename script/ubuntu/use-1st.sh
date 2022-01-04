8c62fd32c16538ebb19aa745e17a72df7250d42e
AB <4b11b4@gmail.com>
2022-01-04 13:45:53 -0800

    wget comes installed. curl not

--- script/ubuntu/use-1st.sh
+++ script/ubuntu/use-1st.sh
@@ -1,4 +1,4 @@
-curl https://raw.githubusercontent.com/4b11b4/dotfiles/master/script/ubuntu/1st.sh | bash
+wget -O - https://raw.githubusercontent.com/4b11b4/dotfiles/master/script/ubuntu/1st.sh | bash
 
 #curl script w args
 #curl https://github.com/4b11b4/dotfiles/blob/master/script/ubuntu/1st.sh | bash -s -- arg1 arg2
