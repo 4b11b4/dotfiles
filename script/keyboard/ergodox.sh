fa8f4007d9108393715124863d621b52cab8e4ae
AB <4b11b4@gmail.com>
2022-01-26 16:54:16 -0800

    ergodox wally install

---
+++ script/keyboard/ergodox.sh
@@ -0,0 +1,16 @@
+# https://github.com/zsa/wally/wiki/Linux-install
+# note: may need to run with sudo
+
+# deps (ubuntu)
+apt install -y libusb-dev
+
+# download wally to pub, make executable
+wget -P ~/pub/ -O wally https://configure.ergodox-ez.com/wally/linux
+chmod +x ~/pub/wally
+
+# create rules file in order to access low-level device (keyboard)
+mv ~/pub/dotfiles/script/keyboard/50-wally.rules /etc/udev/rules.d/
+
+# make sure user part of plugdev group (not always default)
+groupadd plugdev
+usermod -aG plugdev $USER
